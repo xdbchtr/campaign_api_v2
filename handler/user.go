cf8c8f65e578d52844f9799fef819abd0e6d5959
adbchtr <adibbachtiar44@gmail.com>
2022-01-17 14:39:27 +0700

    init project register user endpoint

---
+++ handler/user.go
@@ -0,0 +1,44 @@
+package handler
+
+import (
+	"campaign_api/helper"
+	"campaign_api/user"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userService user.Service) *userHandler {
+	return &userHandler{userService}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+	var input user.RegisterUserInput
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Register Account Failed", http.StatusBadRequest, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+		return
+	}
+
+	newUser, err := h.userService.RegisterUser(input)
+	if err != nil {
+		response := helper.APIResponse("Register Account Failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	formatter := user.FormatUser(newUser, "tokentokentoken")
+
+	response := helper.APIResponse("Account Has Been Registered", http.StatusOK, "success", formatter)
+	c.JSON(http.StatusOK, response)
+
+}
