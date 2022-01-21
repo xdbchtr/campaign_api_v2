50adfd59d5cd4f014b8b94f5de756aeca04e97e9
adbchtr <adibbachtiar44@gmail.com>
2022-01-21 12:49:11 +0700

    jwt validation

--- main.go
+++ main.go
@@ -26,6 +26,23 @@ func main() {
 	userService := user.NewService(userRepository)
 	authService := auth.NewService()
 
+	token, err := authService.ValidateToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxfQ.6DeKtJEsjw70GuqMWlwlWI9E_WUDTJY4yWdDKbIkUis")
+
+	if err != nil {
+		fmt.Println("ERROR")
+		fmt.Println("ERROR")
+		fmt.Println("ERROR")
+	}
+	if token.Valid {
+		fmt.Println("VALID")
+		fmt.Println("VALID")
+		fmt.Println("VALID")
+	} else {
+		fmt.Println("INVALID")
+		fmt.Println("INVALID")
+		fmt.Println("INVALID")
+	}
+
 	userHandler := handler.NewUserHandler(userService, authService)
 
 	router := gin.Default()
